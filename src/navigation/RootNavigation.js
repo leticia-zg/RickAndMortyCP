37d3afae91bd4e773485921763d7de1beb044f6a
Pedro Novais <phenrique101007@gmail.com>
2025-05-19 23:58:19 -0300

    fix root navigation, add root for initial route name

--- src/navigation/RootNavigation.js
+++ src/navigation/RootNavigation.js
@@ -11,7 +11,7 @@ const Stack = createNativeStackNavigator();
 export default function RootNavigation() {
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName="Intro">
+            <Stack.Navigator initialRouteName="Root">
                 <Stack.Screen
                     name="Root"
                     component={BottomTabsNavigator}
